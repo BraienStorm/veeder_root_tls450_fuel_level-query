fe4b8686c11356fd1b294f9c2e59314613749d2b
unknown <eccosoft@gmail.com>
2024-04-24 12:51:52 +0200

    main

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # veeder_root_tls450_fuel_level-query
 Veeder Root tls-450 fuel level query 
+
+##Useage 
+```
+
+$tsl450 = new TSL450('username', 'password', 'ip:port');
+print_r($tsl450->GetTank(1));    
+
+```
+##Output 
+
+Array
+(
+    [TankName] => Evo Diesel
+    [TankId] => 1
+    [TotalVolume] => 17960.024469057902
+    [Ullage] => 27038.975530942102
+    [WaterHeight] => 0
+    [FuelHeight] => 1028.9852641593475
+    [IsManifolded] => soapFALSE
+    [IsDeliveryInProgress] => soapFALSE
+    [FuelFloatLevelPercentage] => 40
+    [WaterFloatLevelPercentage] => 0
+    [HasWarnings] => soapFALSE
+    [HasAlarms] => soapFALSE
+    [IsProbeOut] => soapFALSE
+    [IsStickHeightEnabled] => soapFALSE
+    [StickHeight] => 1028.9852641593475
+    [TotalTCVolume] => 18009.523361913343
+    [WaterVolume] => 0
+    [FuelTemperature] => 12.153013183118523
+    [UserUllage] => 27038.975530942102
+    [UserUllageEnabled] => soapFALSE
+    [UserUllagePercentage] => 90
+    [HasWaterFloat] => soapTRUE
+    [HasDensityProbe] => soapFALSE
+    [Density] => 0
+    [Mass] => 0
+    [HasTcDensity] => soapFALSE
+    [TcDensity] => 0
+    [DeliveredQuantity] => 16031.806422624877
+    [ManifoldedQuantity] => 16031.806422624877
+    [IsWorkingCapacityEnabled] => soapFALSE
+    [WorkingCapacity] => 3.3360034638850619E+261
+)
---
+++ clientTLS450.php
@@ -0,0 +1,73 @@
+<?php
+class TSL450
+{
+    private $username;
+    private $password;
+    private $ip;
+    private $allTanks = array();
+    public function __construct($username = '', $password = '', $ip = '')
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->ip = $ip;
+        $this->Refress();
+    }
+    private function GetAllTanksOverviewInfo()
+    {
+        $loginxml = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns="urn:TLSWebService"><SOAP-ENV:Body><ns:Login></ns:Login></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+        $loginUrl = 'https://' . $this->username . ':' . $this->password . '@' . $this->ip . '/fcgi-bin/FastCGIRequestHandler';
+        $tanksxml = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns="urn:TLSWebService"><SOAP-ENV:Body><ns:GetAllTanksOverviewInfo></ns:GetAllTanksOverviewInfo></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+        $tanksUrl = 'https://' . $this->ip . '/fcgi-bin/FastCGIRequestHandler';
+        $logoutxml = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns="urn:TLSWebService"><SOAP-ENV:Body><ns:Logout></ns:Logout></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+        $validatorUrl = 'https://0:0@' . $this->ip . '/fcgi-bin/FastCGIRequestHandler';
+        $validatorxml = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns="urn:TLSWebService"><SOAP-ENV:Body><ns:GetCredentialValidators></ns:GetCredentialValidators></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $loginUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $loginxml);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $output = curl_exec($ch);
+        $clean_xml = str_ireplace(['SOAP-ENV:', 'SOAP:', 'ns:'], '', $output);
+        $xml = simplexml_load_string($clean_xml);
+        $sessionId = $xml->Body->LoginResponse->sessionId;
+        curl_setopt($ch, CURLOPT_COOKIE, 'TLSSESSION=' . $sessionId);
+        curl_setopt($ch, CURLOPT_URL, $tanksUrl);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $tanksxml);
+        $output = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $logoutxml);
+        $logout = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_COOKIE, '');
+        curl_setopt($ch, CURLOPT_URL, $validatorUrl);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $validatorxml);
+        $valid = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+    public function Refress()
+    {
+        $xml = $this->GetAllTanksOverviewInfo();
+        $clean_xml = str_ireplace(['SOAP-ENV:', 'SOAP:', 'ns:'], '', $xml);
+        $xml = simplexml_load_string($clean_xml);
+        //$this->allTanks = $xml->Body->AllTankOverviewInfo->AllTanks;
+        foreach ($xml->Body->AllTankOverviewInfo->AllTanks as $tank) {
+            $this->allTanks[] = $tank;
+        }
+        print_r($this->allTanks);
+    }
+    public function GetTank($tankId)
+    {
+        foreach ($this->allTanks as $tank) {
+            if ($tank->TankId == $tankId) {
+                $tank = json_decode(json_encode($tank), true);
+                return $tank;
+            }
+        }
+        return null;
+    }
+}
+
+
+
